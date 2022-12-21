f932c7f55362cc122eb03040a681c95d689e9078
chanwit00 <120072936+chanwit00@users.noreply.github.com>
2022-12-21 21:32:43 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const message = "Hello";
+const place = "World" ; 
+
+
+// logging out String
+console.log('Hello World!');
+
+// using substitutions
+console.log('Hello, %s!' , place);
+
+// using a string literal
+console.log(`${message}, ${place}!`)
