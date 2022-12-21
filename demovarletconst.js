7f3f72a20435850ca1945b8204f8873d4c077099
chanwit00 <120072936+chanwit00@users.noreply.github.com>
2022-12-21 22:16:27 +0700

    Create demovarletconst.js

---
+++ demovarletconst.js
@@ -0,0 +1,13 @@
+var name = "chanwit"
+
+console.log(name);
+name = "name World";
+console.log(name);
+
+if (true) {
+    let World = "Hello World";
+    console.log(World);
+}
+
+const fixval = "Fix Value" ;
+console.log(fixval);
