018f43e998e31cc92706a235e1b7ccf0902352ca
chanwit00 <120072936+chanwit00@users.noreply.github.com>
2023-01-22 16:50:53 +0700

    Update 1-function.js

--- 1-function.js
+++ 1-function.js
@@ -10,9 +10,13 @@ function printgoodbye(name){
 }
 
 console.log(printHello.name)
+// // 2.Function Invocation
 
-// 2.Function Invocation
 let result = printHello("chanwit !");
 console.log(result);
 
 
+console.log(printgoodbye.name);
+
+let bye = printgoodbye("Green ");
+console.log(bye);
